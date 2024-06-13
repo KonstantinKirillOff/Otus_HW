89289cc7673660bf2b9d5e2c1b2cd7116440c094
Konstantin Kirillov <kirilloffkostya@gmail.com>
2024-06-13 13:09:13 +0700

    Initial Commit

---
+++ Otus_HW/Otus_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  Otus_HWApp.swift
+//  Otus_HW
+//
+//  Created by Konstantin Kirillov on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Otus_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
